9696a44993691ba0cb918fb056c43efba3e01dd9
KuangPF <1633397286@qq.com>
2023-03-29 23:29:57 +0800

    refactor(user): 重构获取用户列表

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -15,8 +15,11 @@ router
   .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
   .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
 
+// get user list
+router.get('/list', auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
 // get user info by user id
 router.get('/info', auth('getUsers'), validate(userValidation.getUser), userController.getUser);
+// create user
 router.post('/create', auth('manageUsers'), validate(userValidation.createUser), userController.createUser);
 
 module.exports = router;
@@ -31,57 +34,6 @@ module.exports = router;
 /**
  * @swagger
  * /users:
- *   post:
- *     summary: Create a user
- *     description: Only admins can create other users.
- *     tags: [Users]
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - name
- *               - email
- *               - password
- *               - role
- *             properties:
- *               name:
- *                 type: string
- *               email:
- *                 type: string
- *                 format: email
- *                 description: must be unique
- *               password:
- *                 type: string
- *                 format: password
- *                 minLength: 8
- *                 description: At least one number and one letter
- *               role:
- *                  type: string
- *                  enum: [user, admin]
- *             example:
- *               name: fake name
- *               email: fake@example.com
- *               password: password1
- *               role: user
- *     responses:
- *       "201":
- *         description: Created
- *         content:
- *           application/json:
- *             schema:
- *                $ref: '#/components/schemas/User'
- *       "400":
- *         $ref: '#/components/responses/DuplicateEmail'
- *       "401":
- *         $ref: '#/components/responses/Unauthorized'
- *       "403":
- *         $ref: '#/components/responses/Forbidden'
- *
  *   get:
  *     summary: Get all users
  *     description: Only admins can retrieve all users.
