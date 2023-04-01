36422b383d75995d3ccddf06b34ce415213eb844
KuangPF <1633397286@qq.com>
2023-04-01 11:01:40 +0800

    feat(swagger): 采用 yml 文件编写接口文档

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -18,36 +18,3 @@ router.patch('/update', auth('manageUsers'), validate(userValidation.updateUser)
 router.delete('/delete', auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
 
 module.exports = router;
-
-/**
- * @swagger
- * tags:
- *   name: Users
- *   description: User management and retrieval
- */
-
-/**
- * @swagger
- *   delete:
- *     summary: Delete a user
- *     description: Logged in users can delete only themselves. Only admins can delete other users.
- *     tags: [Users]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: User id
- *     responses:
- *       "200":
- *         description: No content
- *       "401":
- *         $ref: '#/components/responses/Unauthorized'
- *       "403":
- *         $ref: '#/components/responses/Forbidden'
- *       "404":
- *         $ref: '#/components/responses/NotFound'
- */
