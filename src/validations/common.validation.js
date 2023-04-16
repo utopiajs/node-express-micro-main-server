d0f80981c4ac3291647ae2a97ad4187f8708668c
KuangPF <1633397286@qq.com>
2023-04-16 12:18:21 +0800

    docs(common): 限制 dayIndex 范围

--- src/validations/common.validation.js
+++ src/validations/common.validation.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const getBingImg = {
   query: Joi.object().keys({
-    dayIndex: Joi.number().integer(),
+    dayIndex: Joi.number().min(0).max(7).integer(),
     days: Joi.number().min(1).max(8).integer()
   })
 };
