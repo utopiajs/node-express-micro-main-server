c11a5becc8d6f4a53e708c2c803e543bfc6987ae
KuangPF <1633397286@qq.com>
2023-04-19 00:09:42 +0800

    feat(common-service): support auth

--- src/routes/v1/common.route.js
+++ src/routes/v1/common.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const auth = require('../../middlewares/auth');
 const commonController = require('../../controllers/common.controller');
 const validate = require('../../middlewares/validate');
 const commonValidation = require('../../validations/common.validation');
 
 const router = express.Router();
 
-router.get('/static/bing-img', validate(commonValidation.getBingImg), commonController.getBingImageList);
+router.get('/static/bing-img', auth(), validate(commonValidation.getBingImg), commonController.getBingImageList);
 
 module.exports = router;
