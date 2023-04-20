75bef07855ac00eadf074a5c8d14ba05b3956bb3
KuangPF <1633397286@qq.com>
2023-04-20 20:29:01 +0800

    feat(controller): set cookie value after login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -13,6 +13,8 @@ const login = catchAsync(async (req, res) => {
   const { email, password } = req.body;
   const user = await authService.loginUserWithEmailAndPassword(email, password);
   const tokens = await tokenService.generateAuthTokens(user);
+  res.cookie('id', user.id);
+  res.cookie('token', tokens.access.token);
   res.send(handleResponse({ user, tokens }));
 });
 
