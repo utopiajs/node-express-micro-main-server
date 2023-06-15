48277095ac9d231de0c191a2f38ddcd53aea96f6
KuangPF <1633397286@qq.com>
2023-06-15 23:56:21 +0800

    feat(utils): 抽离上传七牛云逻辑

---
+++ src/utils/uploadToOSS.js
@@ -0,0 +1,39 @@
+const qiniu = require('qiniu');
+const config = require('../config/config');
+
+const { accessKey, secretKey } = config.oss;
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+
+// 自定义凭证有效期（示例2小时，expires单位为秒，为上传凭证的有效时间）
+const options = {
+  scope: 'oss-utopia-space-sea',
+  returnBody: '{"key":"$(key)","hash":"$(etag)","fsize":$(fsize),"bucket":"$(bucket)","name":"$(x:name)"}',
+  expires: 7200
+};
+const putPolicy = new qiniu.rs.PutPolicy(options);
+const uploadToken = putPolicy.uploadToken(mac);
+
+const ossConfig = new qiniu.conf.Config();
+// 空间对应的机房
+ossConfig.zone = qiniu.zone.Zone_as0;
+
+const uploadToOSS = ({ key, localFile }) => {
+  const formUploader = new qiniu.form_up.FormUploader(ossConfig);
+  const putExtra = new qiniu.form_up.PutExtra();
+
+  return new Promise((reslove, reject) => {
+    formUploader.putFile(uploadToken, key, localFile, putExtra, (respErr, respBody, respInfo) => {
+      if (respErr) {
+        reject(respErr, respBody);
+      }
+
+      if (respInfo.statusCode === 200) {
+        reslove(respBody);
+      } else {
+        reject(respBody);
+      }
+    });
+  });
+};
+
+module.exports = uploadToOSS;
