6ef5c57541a4e0e8f9336250467fc4df36b26e9f
KuangPF <1633397286@qq.com>
2023-06-16 23:59:16 +0800

    chore(header): header `Authorization` 自动从 cookie 中获取

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -14,7 +14,29 @@ router.use('/', swaggerUi.serve);
 router.get(
   '/',
   swaggerUi.setup(specs, {
-    explorer: true
+    swaggerOptions: {
+      requestInterceptor: (request) => {
+        function getCookie(cookieName) {
+          const name = `${cookieName}=`;
+          const decodedCookie = decodeURIComponent(document.cookie);
+          const cookieArray = decodedCookie.split(';');
+
+          for (let i = 0; i < cookieArray.length; i += 1) {
+            let cookie = cookieArray[i];
+            while (cookie.charAt(0) === ' ') {
+              cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(name) === 0) {
+              return cookie.substring(name.length, cookie.length);
+            }
+          }
+          return '';
+        }
+
+        request.headers.Authorization = `Bearer ${getCookie('token')}`;
+        return request;
+      }
+    }
   })
 );
 
